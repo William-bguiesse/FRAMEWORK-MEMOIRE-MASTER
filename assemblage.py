bfcd07936160214d4955895680cda5bfd1c48291
William BE GUIESSE <williambeguiesse@gmail.com>
2026-07-21 14:03:26 +0200

    on assembnos deux fichiers dans un seul

---
+++ assemblage.py
@@ -0,0 +1,9 @@
+import glob 
+# utiisons à nouveau glob pour recuperer les fichiers markdown dans le repertoire courant
+from lecteur import MarkdownReader 
+# importons markdownReader depuis le fichier lecteur.py 
+# pour recuperer le texte en markdown qui a ete lu
+from convertisseur import Convertisseur
+# importons Convertisseur depuis le fichier convertisseur.py, ça servira a convertir le texte lu
+
+
