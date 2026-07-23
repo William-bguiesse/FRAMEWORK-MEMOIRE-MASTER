c8902273bb1c69ff40c070567d95b5b8aba80f6a
William BE GUIESSE <williambeguiesse@gmail.com>
2026-07-23 13:49:30 +0200

    filtrer les fichier sans le fichier README

--- assemblage.py
+++ assemblage.py
@@ -2,22 +2,10 @@ import glob
 from lecteur import MarkdownReader 
 from convertisseur import Convertisseur
 
-# 1. Récupérer tous les fichiers .md du dossier
-tous_les_fichiers = glob.glob("*.md")
+# 1. Récupérer tous les fichiers markdown avec glob.
+fichier_a_lire = glob.glob("*.md")
+fichiers_valides = [f for f in fichier_a_lire if f.lower() != "readme.md"]
+# on règle un problème en excluant le fichier README.md de la liste des fichiers à lire.
 
-# 2. Filtrer pour ignorer README.md (quelle que soit sa casse)
-fichiers_a_lire = [f for f in tous_les_fichiers if f.lower() != "readme.md"]
 
-print(f"Fichiers markdown valides trouvés : {fichiers_a_lire}")
 
-# 3. Si on trouve au moins un fichier, on lit le premier
-if fichiers_a_lire:
-    fichier_cible = fichiers_a_lire[0]
-    print(f"\nLecture du fichier : {fichier_cible}\n" + "-"*30)
-    
-    lecteur = MarkdownReader(fichier_cible)
-    texte_brut = lecteur.read_file()
-    
-    print(texte_brut)
-else:
-    print("Aucun fichier .md à lire (hors README.md).")
