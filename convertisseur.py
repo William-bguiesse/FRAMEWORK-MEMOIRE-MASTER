406ee2e42f1c715523d2619e662a49cd507b0c88
William BE GUIESSE <williambeguiesse@gmail.com>
2026-07-30 01:49:34 +0200

    maj conv json

--- convertisseur.py
+++ convertisseur.py
@@ -17,4 +17,24 @@ class Convertisseur:
 # Elle retourne ensuite le code HTML résultant.
 
 
-    
+class Jsonconvertisseur:
+    def __init__(self, donnees_json):
+        self.donnees = donnees_json
+
+    def conversion_en_html(self) -> str:
+        if isinstance(self.donnees, dict):
+            html = "<div class='json-content'>\n  <ul>\n"
+            for cle, valeur in self.donnees.items():
+                html += f"    <li><strong>{cle}</strong> : {valeur}</li>\n"
+            html += "  </ul>\n</div>"
+            return html
+# pour tous les couples de clé et valeurs de notre fichier JSON, 
+# on crée une liste en HTML grâce aux balises "ul"
+        elif isinstance(self.donnees, list):
+            html = "<div class='json-content'>\n  <ul>\n"
+            for element in self.donnees:
+                html += f"    <li>{element}</li>\n"
+            html += "  </ul>\n</div>"
+            return html
+
+        return f"<div class='json-content'><p>{self.donnees}</p></div>"
